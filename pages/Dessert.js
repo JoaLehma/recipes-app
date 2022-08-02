84ca230054ef29500283447909f84b18b068a72d
Joachim Lehmann <joa.lehmann@web.de>
2022-08-02 14:14:14 +0200

    Create Veggie and Dessert Pages

---
+++ pages/Dessert.js
@@ -0,0 +1,8 @@
+export default function Dessert() {
+  return (
+    <>
+      <h1>Yummie Desserts!</h1>
+      <p>More please!</p>
+    </>
+  );
+}
---
+++ pages/Veggie.js
@@ -0,0 +1,7 @@
+export default function Veggie() {
+  return;
+  <>
+    <h1>Bah, Veggies!</h1>
+    <p>no thanks...</p>
+  </>;
+}
